a9c235fbd9fdc5dc000734e4850e42bfbcc5614b
happyssong <104614068+happyssong@users.noreply.github.com>
2022-12-16 10:26:02 +0900

    Add files via upload

---
+++ bankInfoIbatisTest/src/kr/or/ddit/bank/vo/BankInfoVO.java
@@ -0,0 +1,35 @@
+package kr.or.ddit.bank.vo;
+
+public class BankInfoVO {
+	private String bank_no;
+	private String bank_name;
+	private String bank_user_name;
+	private String bank_date;
+	
+	public String getBank_no() {
+		return bank_no;
+	}
+	public void setBank_no(String bank_no) {
+		this.bank_no = bank_no;
+	}
+	public String getBank_name() {
+		return bank_name;
+	}
+	public void setBank_name(String bank_name) {
+		this.bank_name = bank_name;
+	}
+	public String getBank_user_name() {
+		return bank_user_name;
+	}
+	public void setBank_user_name(String bank_user_name) {
+		this.bank_user_name = bank_user_name;
+	}
+	public String getBank_date() {
+		return bank_date;
+	}
+	public void setBank_date(String bank_date) {
+		this.bank_date = bank_date;
+	}
+	
+	
+}
